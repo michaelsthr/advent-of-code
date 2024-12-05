45c1aa0071b3a14583150a491dd3be467fd7d073
michisthr <michlstoehr19@icloud.com>
2024-12-05 21:54:01 +0100

    add day5.py (part two)

--- 2024/day5.py
+++ 2024/day5.py
@@ -1,6 +1,31 @@
 import re
 
 
+def swap_until_true(order):
+    print(order)
+    for idx, num in enumerate(order):
+        for r in rules:
+            if num == r[0]:
+                for x in range(0, idx - 1):
+                    if r[1] == order[x]:
+                        print(f"{r[1]} is sus")
+                        l_idx = order.index(r[0])
+                        r_idx = order.index(r[1])
+                        order[l_idx], order[r_idx] = order[r_idx], order[l_idx]
+                        print("swap", order[l_idx], order[r_idx])
+                        return swap_until_true(order)
+            if num == r[1]:
+                for x in range(idx, len(order)):
+                    if r[0] == order[x]:
+                        print(f"{r[0]} is sus")
+                        l_idx = order.index(r[0])
+                        r_idx = order.index(r[1])
+                        order[l_idx], order[r_idx] = order[r_idx], order[l_idx]
+                        print("swap", order[l_idx], order[r_idx])
+                        return swap_until_true(order)
+    return order
+
+
 def check_rules(order):
     for idx, num in enumerate(order):
         for r in rules:
@@ -31,9 +56,16 @@ for line in lines:
         orders.append(numbers)
 
 total_sum = 0
+swapped_sum = 0
 for order in orders:
     if check_rules(order) is True:
         middle = int(len(order) / 2) + 1
         total_sum += int(order[middle - 1])
+    else:
+        norder = swap_until_true(order)
+        middle = int(len(norder) / 2) + 1
+        print(f"MIDDLE: {int(norder[middle - 1])}")
+        swapped_sum += int(norder[middle - 1])
 
 print("SUM:", total_sum)
+print("SWAPPED SUM:", swapped_sum)
