61db3bf9de2c384dc9422f21c48b43a0cf893af0
michisthr <michlstoehr19@icloud.com>
2024-12-08 22:00:53 +0100

    add day7.py (part one)

--- 2024/day7.py
+++ 2024/day7.py
@@ -1,67 +1,48 @@
 import logging
 import re
 import time
+from loguru import logger
 
 
-def recursive(full_solution: list, sol: list, vals: list, i):
-    print(i, sol)
-    i += 1
-    if i >= len(vals):
-        # print("limit")
-        return sol
-    next_val = int(vals[i])
-    # full_solution = full_solution
-    # print(sol)
-    tmp = []
-    for s in sol:
-        # print(s)
-        plus = s + next_val
-        mul = s * next_val
+def validate_equation(result, values):
+    solutions = list()
+    solutions += [values[0]]
+    i = 1
 
-        tmp.append(plus)
-        tmp.append(mul)
-        # print("tmp", tmp)
+    while i < len(values):
+        next_value = values[i]
+        tmp_solutions = list()
 
-    sol = tmp
-    sol = tmp
+        for solution in solutions:
+            plus = solution + next_value
+            mul = solution * next_value
+            tmp_solutions += [plus, mul]
+            
+        solutions = tmp_solutions
+        i += 1
 
-    full_solution.append(plus)
-    full_solution.append(mul)
-    print("full_sos", full_solution)
-    # print("sol", sol)
-
-    return full_solution + recursive(
-        full_solution=full_solution, sol=sol, vals=vals, i=i
-    )
-
-
-def check_eq(res, vals):
-    full_solution = []
-    sol = []
-    sol.append(vals[0])
-    print("FIRST SOL:", sol)
-    solution = recursive(full_solution=full_solution, sol=sol, vals=vals, i=0)
-    # print("solution:", solution)
-    if res in solution:
+    if result in solutions:
         return True
     return False
 
 
-with open("2024/temp/input7e.txt") as f:
-    eqs = list(f.read().splitlines())
+if __name__ == "__main__":
+    with open("2024/temp/input7.txt") as f:
+        equation_lines = list(f.read().splitlines())
 
-sums = 0
-for eq in eqs:
-    eq = eq.split(" ")
-    result = re.findall(r"\d+", eq[0])
-    vals = eq[1:]
-    print("RES:", result, "VAL:", vals)
-    vals = [int(x) for x in vals]
-    result = [int(x) for x in result]
-    if check_eq(result[0], vals):
-        sums += result[0]
-        print(result, "is true")
-        continue
-    print(result, "is false")
+    sums = 0
+    for equat in equation_lines:
+        equat = equat.split(" ")
+        result = int(re.findall(r"\d+", equat[0])[0])
+        values = [int(x) for x in equat[1:]]
 
-print("sum:", sums)
+        if validate_equation(result, values):
+            sums += result
+            logger.success("")
+            print("RESULT:", result, "VALUES:", values)
+            continue
+
+        logger.error("")
+        print("RESULT:", result, "VALUES:", values)
+
+    print("sum:", sums)
