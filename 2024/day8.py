90eea0697bab0956cecc6887cdcb88002de764d0
michisthr <michlstoehr19@icloud.com>
2024-12-09 20:31:03 +0100

    need to study linear algebra again

--- 2024/day8.py
+++ 2024/day8.py
@@ -1,35 +1,75 @@
-from enum import Enum
+# find range between antennas pos
+# --> map the anodes on each side
+
+# difference between pos?
 
 
 class Antenna:
-    def __init__(self, symbol, pos):
+    def __init__(self, symbol, vec):
         self.symbol = symbol
-        self.pos = pos
-    
+        self.vec = vec
+        self.y = vec[0]
+        self.x = vec[1]
+
     def __str__(self):
-        return f"{self.symbol}: {self.pos}"
+        return f"{self.symbol}: {self.vec} --> y:{self.y}, x:{self.x}"
 
 
 def print_matrix(matrix):
+    print("  ", end="")
+    for x in range(len(matrix[0])):
+        print(f"{x}", end=" ")
+    print("\n", end="")
     for y in range(len(matrix)):
-        print(*matrix[y])
+        print(y, *matrix[y])
     print("\n")
 
 
-if __name__ == "__main__":
-    with open("2024/temp/input8.txt") as f:
-        matrix = list(f.read().splitlines())
+def add(v1: tuple, v2: tuple):
+    return tuple(map(lambda i, j: i + j, v1, v2))
 
-    antennas = list()
+
+def sub(v1: tuple, v2: tuple):
+    return tuple(map(lambda i, j: i - j, v1, v2))
+
+
+if __name__ == "__main__":
+    with open("2024/temp/input8e2.txt") as x:
+        matrix = list(x.read().splitlines())
+
+    antennas: list[Antenna] = list()
 
     for y in range(len(matrix)):
         matrix[y] = list(matrix[y])
-        for x in range(len(matrix)):
+        for x in range(len(matrix[y])):
             if matrix[y][x] != ".":
                 a = Antenna(matrix[y][x], (y, x))
                 antennas.append(a)
+                print(a)
 
     for a in antennas:
-        print(a)
+        if a.symbol == "b":
+            b = a
+            print("b ant:", b)
+
+    for a in antennas:
+        if a.symbol != "b":
+            y, x = add(a.vec, sub(a.vec, b.vec))
+            print("vec", (y, x))
+            matrix[y][x] = "#"
+
+    # diff1 = abs(antennas[0].pos[0] - antennas[1].pos[0])
+    # diff2 = abs(antennas[0].pos[1] - antennas[1].pos[1])
+
+    # new_pos1 = antennas[1].pos[0] + diff1
+    # new_pos2 = antennas[1].pos[1] + diff2
+
+    # new_pos1b = antennas[0].pos[0] - diff1
+    # new_pos2b = antennas[0].pos[1] - diff2
+
+    # matriy[new_pos1][new_pos2] = "#"
+    # matriy[new_pos1b][new_pos2b] = "#"
+
+    # print((diff1, diff2))
 
     print_matrix(matrix)
