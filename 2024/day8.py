413191255b6701185e873ab231895405f8b0bcb9
michisthr <michlstoehr19@icloud.com>
2024-12-09 22:53:18 +0100

    add working day8.py (part2)

--- 2024/day8.py
+++ 2024/day8.py
@@ -1,7 +1,3 @@
-# find range between antennas pos
-# --> map the anodes on each side
-
-# difference between pos?
 import copy
 
 
@@ -26,12 +22,19 @@ def print_grid(grid):
     print("\n")
 
 
-def add(v1: tuple, v2: tuple):
+def add(v1: tuple, v2: tuple) -> tuple:
     return tuple(map(lambda i, j: i + j, v1, v2))
 
 
-def sub(v1: tuple, v2: tuple):
+def sub(v1: tuple, v2: tuple) -> tuple:
     return tuple(map(lambda i, j: i - j, v1, v2))
+
+
+def mul(n: int, v: tuple) -> tuple:
+    y = n * v[0]
+    x = n * v[1]
+    # print(f"{y, x} = {n} * {v[0]} and {n} * {v[1]}")
+    return (n * v[0], n * v[1])
 
 
 if __name__ == "__main__":
@@ -61,21 +64,59 @@ if __name__ == "__main__":
             grouped_antennas.append(tmp)
 
     unique_loc = list()
+    unique_loc2 = list()
 
     for antenna in grouped_antennas:
         for i, a in enumerate(antenna):
+            j, l = a.vec
+            # grid[j][l] = "#"
+            unique_loc2.append(a.vec)
             tmp_antennas = copy.deepcopy(antenna)
             tmp_antennas.pop(i)
             for b in tmp_antennas:
-                y, x = add(b.vec, sub(b.vec, a.vec))
+                c = sub(b.vec, a.vec)
+                y, x = add(b.vec, c)
+                vec = (y, x)
                 # print(f"{a.symbol}: {(y, x)} = add({b.vec}, sub({b.vec}, {a.vec}))")
                 if y >= 0 and x >= 0 and y < len(grid) and x < len(grid[0]):
                     # print("add antinode", (y, x))
                     unique_loc.append((y, x))
                     grid[y][x] = "#"
-                # else:
-                # print("skip antinode:", (y, x))
+                    # go up
+                    new_vec = copy.deepcopy(vec)
+                    ik = 1
+                    while True:
+                        print(f"il = {ik}")
+                        y, x = add(b.vec, mul(ik, c))
+                        # print(mul(ik, new_vec), "+", b.vec, "=", (y, x))
+                        if y >= 0 and x >= 0 and y < len(grid) and x < len(grid[0]):
+                            unique_loc2.append((y, x))
+                            grid[y][x] = "#"
+                            ik += 1
+                        else:
+                            print("skip", (y, x))
+                            break
+                    # go down
+                    new_vec = copy.deepcopy(vec)
+                    il = -1
+                    while True:
+                        print(f"il = {il}")
+                        y, x = add(b.vec, mul(il, c))
+                        # print(mul(il, new_vec), "+", b.vec, "=", (y, x))
+                        if y >= 0 and x >= 0 and y < len(grid) and x < len(grid[0]):
+                            unique_loc2.append((y, x))
+                            grid[y][x] = "#"
+                            il -= 1
+                        else:
+                            print("skip", (y, x))
+                            break
+            print_grid(grid)
 
+            # else:
+            # print("skip antinode:", (y, x))
+
+    alle = unique_loc + unique_loc2
     sum_unique_loc = len(set(unique_loc))
-    print_grid(grid)
+    sum_unique_loc2 = len(set(alle))
     print("Unique locations containing antinodes:", sum_unique_loc)
+    print("Unique locations containing antinodes (part 2):", sum_unique_loc2)
