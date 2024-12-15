06972782458c8859745499b6f5350891149d3efa
michisthr <michlstoehr19@icloud.com>
2024-12-15 20:03:58 +0100

    add own caching :)

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,28 +1,28 @@
 import re
-import functools
 
 cache = {}
 
 
-@functools.cache
 def blink(stone, i):
     result = 0
 
     if i == 0:
         return 1
 
-    if stone == 0:
-        result += blink(1, i-1)
-    elif len(str(stone)) % 2 == 0:
-        tmp = str(stone)
-        n = len(tmp) // 2
-        left = tmp[:-n]
-        right = tmp[-n:]
-        result += blink(int(left), i-1) + blink(int(right), i-1)
-    else:
-        result += blink(stone * 2024, i-1)
+    if (str(stone) + "." + str(i)) not in cache:
+        if stone == 0:
+            result += blink(1, i-1)
+        elif len(str(stone)) % 2 == 0:
+            tmp = str(stone)
+            n = len(tmp) // 2
+            left = tmp[:-n]
+            right = tmp[-n:]
+            result += blink(int(left), i-1) + blink(int(right), i-1)
+        else:
+            result += blink(stone * 2024, i-1)
+        cache[str(stone) + "." + str(i)] = result
 
-    return result
+    return cache[str(stone) + "." + str(i)]
 
 
 if __name__ == "__main__":
@@ -33,8 +33,7 @@ if __name__ == "__main__":
     stones = [int(x) for x in stones]
 
     count = 0
-    print("init count", count)
     for stone in stones:
         count += blink(stone, 75)
 
-    print(f"They are {count} stones")
+    print(f"There are {count} stones")
